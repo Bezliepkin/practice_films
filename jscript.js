136891191060774edc35e7b0e66d00009a1d3851
Bezliepkin Maksym <maxim.bezliepkin@gmail.com>
2021-01-28 01:35:21 +0200

    Циклы. Проверка введенных данных
    В консоль выводится комментарий, в зависимости от кол-ва просмотренных фильмов, также производится проверка введенных данных на корректность

--- jscript.js
+++ jscript.js
@@ -12,9 +12,27 @@ const personalMovieDb = {
 
 for (let i = 0; i<numberOfFilms; i++) {
     const a = prompt("Назовите один из недавно просмотренных фильмов?", ''),
-      b = prompt("Какой рейтинг Вы бы дали этому фильму?", '');
+          b = prompt("Какой рейтинг Вы бы дали этому фильму?", '');
 
-      personalMovieDb.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length <= 3) {
+        personalMovieDb.movies[a] = b;
+
+    }else  {
+        console.log('error with film Data');
+        i--;
+    }
 }
+
+if (personalMovieDb.count < 5) {
+    console.log("Просмотрено довольно мало фильмов..");
+}else if (personalMovieDb.count >= 5 && personalMovieDb.count <= 10) {
+    console.log("Отлично, но соседи смотрят больше фильмов!:)");
+}else if (personalMovieDb.count >10) {
+    console.log("Да Вы киноман!!!");
+}else {
+    console.log('error with film count Data');
+
+}
+
 
 console.log(personalMovieDb);
